10ea0bae3592d97ce50200c0adfee8d8f0bad564
Amit <ak9425391@gmail.com>
2026-03-11 12:38:00 +0530

    fixes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -39,8 +39,8 @@ app.use((req, res, next) => {
   res.set('Pragma', 'no-cache');
   res.set('Expires', '0');
   res.set('Surrogate-Control', 'no-store');
-  res.set('Cache-Control', ';
-  next();
+  res.set('Cache-Control',
+    next();
 });
 
 app.use(clerkMiddleware());
